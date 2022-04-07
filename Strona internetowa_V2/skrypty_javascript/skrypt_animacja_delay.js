1b78478f9847ccb9512027cbdb692791fa1c7a9b
Michał Gotner <80587726+MG814@users.noreply.github.com>
2022-04-07 11:09:08 +0200

    Projekt strony www v 2.0

---
+++ Strona internetowa_V2/skrypty_javascript/skrypt_animacja_delay.js
@@ -0,0 +1,26 @@
+function pokaz()
+{
+new Effect.Move('element_3', { 
+y: 1020,
+duration: 5,
+transition: Effect.Transitions.spring,
+delay: 1});
+
+new Effect.Move('element_4', { 
+y: 1020,
+duration: 5,
+transition: Effect.Transitions.spring,
+delay: 5});
+
+new Effect.Move('element_5', { 
+y: 1020,
+duration: 5,
+transition: Effect.Transitions.spring,
+delay: 10});
+
+
+}
+
+
+
+
---
+++ Strona internetowa_V2/skrypty_javascript/skrypt_effect_toggle.js
@@ -0,0 +1,6 @@
+function pokaz_ukryj()
+{
+Effect.toggle('element_2', 'blind', { delay: 0.5, duration: 1.0, transition:Effect.Transitions.sinoidal });
+}
+
+
